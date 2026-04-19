e16bab22b538493eca5afb0c90956c5cf3a692d9
mLeonelZ <matheus.leoonel@gmail.com>
2026-04-19 12:03:03 -0300

    test: testes da aplicação

---
+++ src/test/java/br/com/helpchamados/HelpChamadosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.helpchamados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpChamadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
